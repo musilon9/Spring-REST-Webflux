fc3e38a91be02680e595e87977becf892e6291db
Unknown <musilon9@fel.cvut.cz>
2018-10-21 18:20:16 +0200

    RestController for Categories

---
+++ src/main/java/org/oneionn/spring/spring5webfluxrest/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package org.oneionn.spring.spring5webfluxrest.controller;
+
+import org.oneionn.spring.spring5webfluxrest.domain.Category;
+import org.oneionn.spring.spring5webfluxrest.repository.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Ondrej Musil
+ */
+@RequestMapping("api/v1/categories")
+@RestController()
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    Flux<Category> list() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Mono<Category> getById(@PathVariable String id) {
+        return categoryRepository.findById(id);
+    }
+}
