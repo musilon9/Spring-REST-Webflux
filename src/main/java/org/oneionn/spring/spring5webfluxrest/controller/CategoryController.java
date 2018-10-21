7b316c7bf1d7a33df5b0c8dc0a24db14ebf29482
Unknown <musilon9@fel.cvut.cz>
2018-10-21 19:07:17 +0200

    POST example- Create Category + Test

--- src/main/java/org/oneionn/spring/spring5webfluxrest/controller/CategoryController.java
+++ src/main/java/org/oneionn/spring/spring5webfluxrest/controller/CategoryController.java
@@ -2,9 +2,14 @@ package org.oneionn.spring.spring5webfluxrest.controller;
 
 import org.oneionn.spring.spring5webfluxrest.domain.Category;
 import org.oneionn.spring.spring5webfluxrest.repository.CategoryRepository;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -27,8 +32,15 @@ public class CategoryController {
         return categoryRepository.findAll();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    Mono<Void> create(@RequestBody Publisher<Category> categoryStream) {
+        return categoryRepository.saveAll(categoryStream).then();
+    }
+
     @GetMapping("/{id}")
     Mono<Category> getById(@PathVariable String id) {
         return categoryRepository.findById(id);
     }
+
 }
--- src/test/java/org/oneionn/spring/spring5webfluxrest/controller/CategoryControllerTest.java
+++ src/test/java/org/oneionn/spring/spring5webfluxrest/controller/CategoryControllerTest.java
@@ -6,11 +6,12 @@ import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.oneionn.spring.spring5webfluxrest.domain.Category;
 import org.oneionn.spring.spring5webfluxrest.repository.CategoryRepository;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 /**
  * @author Ondrej Musil
@@ -38,8 +39,20 @@ public class CategoryControllerTest {
 
         webTestClient.get().uri("api/v1/categories")
                 .exchange()
-                .expectBodyList(Category.class)
-                .hasSize(2);
+                .expectBodyList(Category.class).hasSize(2);
+    }
+
+    @Test
+    public void testCreate() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().description("ddd").build()));
+
+        Mono<Category> categoryMono = Mono.just(Category.builder().description("SomeCat").build());
+
+        webTestClient.post().uri("api/v1/categories")
+                .body(categoryMono, Category.class)
+                .exchange()
+                .expectStatus().isCreated();
     }
 
     @Test
